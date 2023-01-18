ab4218e8f6307a8051bef6f3194a212acef9c5f3
siriusjt99 <103328270+siriusjt99@users.noreply.github.com>
2023-01-18 22:01:51 +0530

    come

--- chat-app/src/Chat.js
+++ chat-app/src/Chat.js
@@ -1,6 +1,8 @@
 import {AttachFile,Message,MoreVert,SearchOutlined} from "@material-ui/icons";
 import {Avatar , IconButton}from "@material-ui/core";
 import React from 'react';
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import "./Chat.css";
 function Chat() {
   return (
@@ -24,13 +26,79 @@ function Chat() {
       </div>
      </div>
      <div className="chat__body">
-      <p>
+      <p className="chat__message">
         <span className="chat__name">Kookie</span>
         This is a message
         <span className="chat__timestamp">
           {new Date().toUTCString()}
         </span>
         </p>
+
+        <p className="chat__message">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+        <p className="chat__message">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+        <p className="chat__message">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+        <p className="chat__message">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+ <p className="chat__message">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+        <p className="chat__message">
+        <span className="chat__name">Kookie</span>
+        This is a message
+        <span className="chat__timestamp">
+          {new Date().toUTCString()}
+        </span>
+        </p>
+     </div>
+
+
+     <div className="chat__footer">
+      <InsertEmoticonIcon/>
+      <form>
+        <input placeholder="Type message" type="text"
+        />
+        <button type="submit">
+          Send a message
+        </button>
+        <MicIcon/>
+        
+       
+      </form>
      </div>
     </div>
   )
